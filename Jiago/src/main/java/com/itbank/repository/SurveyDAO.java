ed7c47ca1721731fe8f98a82776a8f0a46ded53f
unseong <115051363+un-seong@users.noreply.github.com>
2023-02-10 17:22:49 +0900

    [modify] survey_start(home)
    집와서 다시 수정

--- Jiago/src/main/java/com/itbank/repository/SurveyDAO.java
+++ Jiago/src/main/java/com/itbank/repository/SurveyDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.itbank.model.AnswerDTO;
 import com.itbank.model.SurveyDTO;
 import com.itbank.model.SurveyExampleDTO;
 import com.itbank.model.SurveyQuestionDTO;
@@ -12,17 +13,23 @@ import com.itbank.model.SurveyQuestionDTO;
 @Repository
 public interface SurveyDAO {
 
-	List<SurveyDTO> selectList();
+   List<SurveyDTO> selectList();
 
-	SurveyDTO selectOneDetail(int survey_idx);
+   SurveyDTO selectOneDetail(int survey_idx);
 
-	List<SurveyQuestionDTO> selectSurveyQuestion(int survey_idx);
+   List<SurveyQuestionDTO> selectSurveyQuestion(int survey_idx);
 
-	List<SurveyExampleDTO> selectSurveyExample(int survey_idx);
+   List<SurveyExampleDTO> selectSurveyExample(int survey_idx);
 
-	int insertAnswer(HashMap<String, Object> ob);
+   int insertAnswer(HashMap<String, String> map);
+
+   List<Integer> selectQuestionIdx(HashMap<String, String> map);
+
+   int insertAnswerSubstr(HashMap<String, Object> resultMap);
+
+   int deleteAnswerResult(HashMap<String, Object> resultMap);
 
 
 
-	
+   
 }
